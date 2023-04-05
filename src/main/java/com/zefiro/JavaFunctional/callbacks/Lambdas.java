08f4fff20271a1bc838379b1211497b46a4369c0
RiccardoTobia <riccardo.tobia96@gmial.com>
2023-04-05 16:24:44 +0200

    proviamo conflitti

--- src/main/java/com/zefiro/JavaFunctional/callbacks/Lambdas.java
+++ src/main/java/com/zefiro/JavaFunctional/callbacks/Lambdas.java
@@ -6,9 +6,12 @@ public class Lambdas {
 
 	public static void main(String[] args) {
 //		Function<String, String> printName = name -> name.toUpperCase();
-		Function<String, String> toUpperCase = name ->{
-			if(name.isEmpty()) throw new IllegalArgumentException();
-			return name.toUpperCase();
-		};	
+//		Function<String, String> toUpperCase = name ->{
+//			if(name.isEmpty()) throw new IllegalArgumentException();
+//			return name.toUpperCase();
+		
+		//Ciao riccardo del futuro, se mi vedi ed è tutto commentanto,
+		//ricordati che è solo per provare conflitti dvsajfdslòk
+//		};	
 	}
 }
